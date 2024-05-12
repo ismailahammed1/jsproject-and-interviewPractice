dcdabd393c6f1ed21a93ab030aab4bdb7fb8c1e3
ismailahammed1 <ismailahmmedroman@gmail.com>
2024-05-12 15:24:16 +0600

    use if else color update for value

--- Conter/Counter.js
+++ Conter/Counter.js
@@ -1,22 +1,25 @@
-const value=document.getElementById('value');
-const btns=document.querySelectorAll('.btn');
+const value = document.getElementById('value');
+const btns = document.querySelectorAll('.btn');
 
 let counter = 0;
 
 btns.forEach(function(btn) {
     btn.addEventListener('click', function(e) {
-        const calculet=e.currentTarget.classList;
-      if (calculet.contains('Increase')) {
-        counter++;
-      } else if(calculet.contains('Decrease')) {
-        counter--;
-      }else{
-        counter=0;
-      }
-      value.textContent=counter;
+        const calculet = e.currentTarget.classList;
+        if (calculet.contains('Increase')) {
+            counter++;
+        } else if (calculet.contains('Decrease')) {
+            counter--;
+        } else {
+            counter = 0;
+        }
+        if (counter < 0) {
+            value.style.color = "red";
+        } else if (counter > 0) {
+            value.style.color = "green";
+        } else {
+            value.style.color = "black";
+        }
+        value.textContent = counter;
     });
 });
-
-// const Increase=document.querySelector('Increase');
-// const Reset=document.querySelector('Reset');
-// const Decrease=document.querySelector('Decrease');
