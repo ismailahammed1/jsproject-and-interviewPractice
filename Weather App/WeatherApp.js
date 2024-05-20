0302e54e5e8c5c57677c040ffca3f98c16c1d1e2
ismailahammed1 <ismailahmmedroman@gmail.com>
2024-05-20 16:02:09 +0600

    function some issue not solve

--- Weather App/WeatherApp.js
+++ Weather App/WeatherApp.js
@@ -1,9 +1,22 @@
 const apikey="ea23dd0fac9acdf688b018c812a48a97";
-const apiWeatherUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=dhaka"
+const apiWeatherUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=London"
+
+const searchBox=document.querySelector(".search input")
+const searchBtn=document.querySelector(".search button")
 
 async function checkWeather() {
     const response = await fetch(apiWeatherUrl + `&appid=${apikey}`);
     const data = await response.json();
     console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = `${Math.round(data.main.temp)}°C`;
+    document.querySelector(".humidity").innerHTML = `${data.main.humidity}%`;
+    document.querySelector(".wind").innerHTML = `${data.wind.speed} km/h`;
 }
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
 checkWeather()
+
